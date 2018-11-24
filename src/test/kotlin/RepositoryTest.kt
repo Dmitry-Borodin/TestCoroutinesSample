bfa3e05100c22f1e4e7a5e515ef5b42a7fb68a63
Dmitry Borodin <mail@DmitryBorodin.com>
2018-11-24 14:11:38 +0100

    test added

---
+++ src/test/kotlin/RepositoryTest.kt
@@ -0,0 +1,17 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * @author Dmitry Borodin on 11/24/18.
+ */
+class RepositoryTest {
+
+    @Test
+    fun getDataFromNetwork() {
+
+    }
+
+    @Test
+    fun getDataFromDb() {
+    }
+}
