fbc022fb6199cc731b7ca73289575f30c45aa189
Dmitry Borodin <mail@DmitryBorodin.com>
2018-11-24 14:05:38 +0100

    pseudo logic is implemented

--- src/main/kotlin/Presentation.kt
+++ src/main/kotlin/Presentation.kt
@@ -1,3 +1,7 @@
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
 /**
  * @author Dmitry Borodin on 11/24/18.
  */
@@ -5,4 +9,16 @@
 
 fun main(args: Array<String>) {
 
+    println("Starting out program, fetching data")
+    GlobalScope.async {
+        launch {
+            val dbResponse = Repository.getDataFromDb()
+            println("Response from DB is $dbResponse")
+        }
+        launch {
+            val networkResponse = Repository.getDataFromNetwork()
+            println("Response from network is $networkResponse")
+        }
+    }
+    Thread.sleep(3000)
 }
--- src/main/kotlin/Repository.kt
+++ src/main/kotlin/Repository.kt
@@ -1,7 +1,20 @@
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+
 /**
  * @author Dmitry Borodin on 11/24/18.
  */
 
 object Repository {
 
+    suspend fun getDataFromNetwork(): String = withContext(Dispatchers.IO) {
+        delay(2000) ///network request
+        return@withContext "response"
+    }
+
+    suspend fun getDataFromDb(): String = withContext(Dispatchers.IO) {
+        Thread.sleep(1000) //complex sql query
+        return@withContext "response"
+    }
 }
